994ee9fece2aae41b5a5dde24ea657a60938e7f4
Eddie Shin <eshinhw@gmail.com>
2024-04-30 21:44:04 -0400

    Refactored code base

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
-let res = 0;
-let displayStr = "";
+// let res = 0;
+// let displayStr = "";
 
-document.querySelector("#clear").addEventListener("click", clear);
-let displayVal = document.querySelector("#value");
+// document.querySelector("#clear").addEventListener("click", clear);
+// let displayVal = document.querySelector("#value");
 
-document.querySelectorAll(".buttons button").forEach((btn) => {
-  btn.addEventListener("click", check);
-});
+// document.querySelectorAll(".buttons button").forEach((btn) => {
+//   btn.addEventListener("click", check);
+// });
 
-function updateDisplay(newStr) {
-  displayStr += newStr;
-  displayVal.innerHTML = displayStr;
-}
+// function updateDisplay(newStr) {
+//   displayStr += newStr;
+//   displayVal.innerHTML = displayStr;
+// }
+
+// function clear() {
+//   displayStr = "";
+//   displayVal.innerHTML = "";
+// }
+
+// function add() {}
+
+// function check(val) {
+//   let currVal = val.target.innerHTML;
+//   updateDisplay(currVal);
+//   if (val.target.id !== "") {
+//     // math symbol
+//     if (currVal === "C") {
+//       clear();
+//     } else if (currVal === "(") {
+//     } else if (currVal === ")") {
+//     } else if (currVal === "/") {
+//     } else if (currVal === "X") {
+//     } else if (currVal === "-") {
+//     } else if (currVal === "+") {
+//     } else if (currVal === "%") {
+//     } else if (currVal === "=") {
+//     } else if (currVal === ".") {
+//     }
+//   } else {
+//     // number
+//     let currNum = Number(currVal);
+//     console.log(currNum);
+//   }
+// }
+
+let currVal = document.querySelector("#value");
+
+document.querySelector("#clear-btn").addEventListener("click", clear);
+document.querySelector("#del-btn").addEventListener("click", cancel);
+
+let displayVal = "";
+let outputVal = undefined;
 
 function clear() {
-  displayStr = "";
-  displayVal.innerHTML = "";
+  currVal.innerHTML = "";
 }
 
-function add() {}
-
-function check(val) {
-  let currVal = val.target.innerHTML;
-  updateDisplay(currVal);
-  if (val.target.id !== "") {
-    // math symbol
-    if (currVal === "C") {
-      clear();
-    } else if (currVal === "(") {
-    } else if (currVal === ")") {
-    } else if (currVal === "/") {
-    } else if (currVal === "X") {
-    } else if (currVal === "-") {
-    } else if (currVal === "+") {
-    } else if (currVal === "%") {
-    } else if (currVal === "=") {
-    } else if (currVal === ".") {
-    }
-  } else {
-    // number
-    let currNum = Number(currVal);
-    console.log(currNum);
-  }
+function cancel() {
+  currVal.innerHTML = currVal.innerHTML.toString().slice(0, -1);
 }
